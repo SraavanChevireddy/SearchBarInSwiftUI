06a9e3a9d30c2b1cb04c13bf8adfc6351bdfa576
Sraavan Chevireddy <schevireddy@saventech.com>
2020-11-21 00:04:18 +0530

    Added basic UI

--- SearchBarInSwiftUI/ContentView.swift
+++ SearchBarInSwiftUI/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var searchText = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ScrollView{
+                HStack{
+                    TextField("Search", text: $searchText)
+                }
+                .padding()
+                .background(Color(.systemGray4))
+                .cornerRadius(12)
+                .padding(.horizontal)
+                
+                ForEach(0..<20, id:\.self){ num in
+                    HStack{
+                        Text("\(num)"); Spacer()
+                    }
+                    .padding()
+                    Divider()
+                        .padding(.leading)
+                }
+            }
+            .navigationTitle("Food")
+        }
     }
 }
 
