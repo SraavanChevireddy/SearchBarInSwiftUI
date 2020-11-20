de37ebad33be19830c8b7db904fcd8aeff4e26a5
Sraavan Chevireddy <schevireddy@saventech.com>
2020-11-20 23:50:19 +0530

    Initial Commit

---
+++ SearchBarInSwiftUI/SearchBarInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarInSwiftUIApp.swift
+//  SearchBarInSwiftUI
+//
+//  Created by Sraavan Chevireddy on 11/20/20.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
